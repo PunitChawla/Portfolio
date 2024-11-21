6339c3a1272959b2f036daab1be6c2627935a14c
PunitChawla <chawlapunit79@gmail.com>
2024-11-22 00:15:11 +0530

    add social link

--- src/section/Footer.jsx
+++ src/section/Footer.jsx
@@ -8,8 +8,8 @@ export const Footer = ()=>{
             </div>
             <div className="flex gap-3">
                 <div className="social-icon">
-                    <a  href="https://github.com/PunitChawla"  target = '_blank' className="flex jusfity-center">
-                      <img src="/assets/github.svg" alt="github"  className="w-full h-full " />
+                    <a  href="https://www.linkedin.com/in/punit-chawla-91b186241"  target = '_blank' className="flex jusfity-center">
+                      <img src="/assets/linkedIn_PNG8.png" alt="github"  className="w-full h-full " />
                     </a>
                 </div>
                 <div className="social-icon">
@@ -18,8 +18,8 @@ export const Footer = ()=>{
                     </a>
                 </div>
                 <div className="social-icon">
-                    <a  href="https://github.com/PunitChawla"  target = '_blank' className="flex jusfity-center">
-                      <img src="/assets/github.svg" alt="github"  className="w-full h-full " />
+                    <a  href="https://leetcode.com/Punit_chawla/"  target = '_blank' className="flex jusfity-center">
+                      <img src="/assets/LeetCode_logo.png" alt="github"  className="w-full h-full " />
                     </a>
                 </div>
             </div>
